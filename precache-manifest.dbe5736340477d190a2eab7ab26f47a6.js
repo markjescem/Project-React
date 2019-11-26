6b4700aaf0cda2efaf4d2c0a971c6a52964bc94d
Jescem <mangalay03@gmail.com>
2019-11-26 17:10:54 +0800

    Updates

---
+++ precache-manifest.dbe5736340477d190a2eab7ab26f47a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa69bafa41275167fca28a337c7d695d",
+    "url": "/Project-React/index.html"
+  },
+  {
+    "revision": "86b9cb659107c9e2e92f",
+    "url": "/Project-React/static/css/main.dfe17f73.chunk.css"
+  },
+  {
+    "revision": "83cf294537b111a6187b",
+    "url": "/Project-React/static/js/2.b89aec13.chunk.js"
+  },
+  {
+    "revision": "86b9cb659107c9e2e92f",
+    "url": "/Project-React/static/js/main.7d2fc0be.chunk.js"
+  },
+  {
+    "revision": "ac5ba39dc77dc78494eb",
+    "url": "/Project-React/static/js/runtime-main.862ac112.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/Project-React/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/Project-React/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/Project-React/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/Project-React/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/Project-React/static/media/item6.f5fd97b5.jpg"
+  }
+]);
